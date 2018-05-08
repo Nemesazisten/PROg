35123864ce6f47ff82538dd03b92f3ba864bc52a
Nemesazisten <39067466+Nemesazisten@users.noreply.github.com>
2018-05-08 22:22:32 +0200

    Create valtcserkulonbseg.cpp

---
+++ valtcserkulonbseg.cpp
@@ -0,0 +1,20 @@
+//tutor Kalmar Patrik
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a=74;
+	int b=98;
+
+	cout<<a<<" "<<b<<endl;
+
+	b=a+b;
+	a=b-a;
+	b=b-a;
+
+	cout<<a<<" "<<b<<endl;
+  
+	return 0;
+}
