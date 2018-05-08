a3e2a5d74905bd5dfefdb94960b44e0a2d8a913e
Nemesazisten <39067466+Nemesazisten@users.noreply.github.com>
2018-05-09 01:35:11 +0200

    Create bogomips.c

---
+++ bogomips.c
@@ -0,0 +1,18 @@
+// tutor Kalmár patrik
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+   FILE *cpuinfo = fopen("/proc/cpuinfo", "rb");
+   char *arg = 0;
+   size_t size = 0;
+   while(getdelim(&arg, &size, 0, cpuinfo) != -1)
+   {
+      puts(arg);
+   }
+   free(arg);
+   fclose(cpuinfo);
+   return 0;
+}
